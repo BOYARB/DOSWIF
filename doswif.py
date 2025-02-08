f075445ce4c9a38cfc4a94c3895f6d493654a205
BO YARB <zomlong9@gmail.com>
2025-02-08 22:54:37 +0300

    Update doswif.py

--- doswif.py
+++ doswif.py
@@ -7,7 +7,7 @@ import sys
 import signal
 
 def handle_ctrl_z(signal, frame):
-    print("\n\033[1;97mStopped\033[0m")  
+    print("\033[1;97mStopped\033[0m", end="")  # طباعة الرسالة دون سطر فارغ
     sys.exit(0)
 
 def handle_ctrl_c(signal, frame):
